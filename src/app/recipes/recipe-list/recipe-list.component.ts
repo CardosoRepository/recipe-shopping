8a0f059a003f3dda22d5d9ddce1ef95d204cdd02
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-02-23 17:16:46 -0300

    Implementing recipes through components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,8 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.scss'
 })
 export class RecipeListComponent {
+	@Output() recipeWasSelected = new EventEmitter<Recipe>();
+
 	recipes: Recipe[] = [
 		new Recipe('A Test Recipe', 'This is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
 		new Recipe('Second Recipe', 'Second test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
 	];
+
+	onRecipeSelected(recipe: Recipe) {
+		this.recipeWasSelected.emit(recipe);
+	}
 }
