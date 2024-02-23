9d073ab7c1bc285e57e69e82dea9f4617734f9ed
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-02-23 16:48:48 -0300

    Data binding to recipe-item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,9 +9,6 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent {
 	recipes: Recipe[] = [
 		new Recipe('A Test Recipe', 'This is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1577/20210517-Pasta-alla-Gricia-with-Lobster3010-1024x576-c.jpg'),
+		new Recipe('Second Recipe', 'Second test', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
 	];
-
 }
