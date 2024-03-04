7142eb1a73f1c5941897fa7a98d81fac47a8b2f8
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-03-04 14:53:56 -0300

    router link active at header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
 	selector: 'app-header',
-	templateUrl: './header.component.html'
+	templateUrl: './header.component.html',
+	styles: ['.active {background-color: rgba(0, 0, 0, 0.05); font-weight: 500}'],
 })
-export class HeaderComponent {
-}
+export class HeaderComponent {}
