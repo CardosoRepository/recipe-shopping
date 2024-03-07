816d5a7c94ec6cc08942619d30502b8a36a3cee1
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-03-07 15:13:01 -0300

    Improving app changing event emitter to subject

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Recipe } from './recipe.model';
 import { RecipeService } from './recipe.service';
 
 @Component({
@@ -9,11 +8,7 @@ import { RecipeService } from './recipe.service';
 	providers: [RecipeService],
 })
 export class RecipesComponent {
-	selectedRecipe: Recipe;
 
-	constructor(private _recipeService: RecipeService) {
-		this._recipeService.recipeSelected.subscribe((recipe: Recipe) => {
-			this.selectedRecipe = recipe;
-		});
+	constructor() {
 	}
 }
