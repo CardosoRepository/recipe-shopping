753bdc298658cc3d8c4ffd3ca461395532d9cd70
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-03-27 12:00:53 -0300

    Implementing ingredient addition by ngForm on shopping-edit

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -8,15 +9,13 @@ import { ShoppingListService } from '../shopping-list.service';
   styleUrl: './shopping-edit.component.scss'
 })
 export class ShoppingEditComponent {
-	@ViewChild('nameInput', { static: false }) nameInputRef: ElementRef;
-	@ViewChild('amountInput', { static: false }) amountInputRef: ElementRef;
 
 	constructor(private _shoppingListService: ShoppingListService) {}
 
-	onAddItem() {
-		const ingredientName = this.nameInputRef.nativeElement.value;
-		const ingredientAmount = this.amountInputRef.nativeElement.value;
-		const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+	onAddItem(form: NgForm) {
+		const formValue = form.value;
+
+		const newIngredient = new Ingredient(formValue.name, formValue.amount);
 		this._shoppingListService.addIngredient(newIngredient);
 	}
 }
