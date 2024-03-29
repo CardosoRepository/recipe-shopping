1fb438792f8383a42bd1d13305a2f42d642b7c2e
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-03-29 19:58:35 -0300

    Store data with Firebase  implemented

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styles: ['.active {background-color: rgba(0, 0, 0, 0.05); font-weight: 500; padding: 0.5rem}'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+	constructor(private _dataStorageService: DataStorageService) {}
+
+	onSaveData() {
+		this._dataStorageService.storeRecipes();
+	}
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,17 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable()
 export class DataStorageService {
-	constructor(private _httpClient: HttpClient) {}
+	constructor(private _httpClient: HttpClient,
+				private _recipeService: RecipeService) {}
+
+	storeRecipes() {
+		const recipes = this._recipeService.getRecipes();
+
+		this._httpClient.put('https://ng-complete-guide-437a4-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(response => {
+			console.log(response);
+		});
+	}
 }
