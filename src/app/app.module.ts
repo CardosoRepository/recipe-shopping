e7b8c5072ec1f683d3c477ea6e5f2345748cde05
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-03 17:04:10 -0300

    Auth Module implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.component';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
-import { AuthComponent } from './auth/auth.component';
 import { AuthService } from './auth/auth.service';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		HeaderComponent,
-		AuthComponent,
 	],
 	imports: [
 		BrowserModule,
-		FormsModule,
 		AppRoutingModule,
-		ReactiveFormsModule,
 		HttpClientModule,
 		RecipesModule,
 		ShoppingListModule,
+		AuthModule,
 		SharedModule
 	],
 	providers: [
