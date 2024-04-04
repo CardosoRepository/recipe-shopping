c2f9016654da6157317906187dc305dd0c360830
Gabriel Cardoso Barbosa <gabrielcard0s0@hotmail.com>
2024-04-04 00:21:23 -0300

    Generating environment files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: false,
+	firebaseAPIKey: 'AIzaSyCP79vHyhPGAR8FYTL5YcO1x75OmqqID5M',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+	production: true,
+	firebaseAPIKey: 'AIzaSyCP79vHyhPGAR8FYTL5YcO1x75OmqqID5M',
+};
